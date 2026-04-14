bb6d341b753416a6c6ac7743aaf6e98799989afd
Loi160 <2311102280@ittelkom-pwt.ac.id>
2026-04-14 13:37:25 +0700

    Upload praktikum11 laravel

---
+++ praktikum11/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::post('/auth', [SiteController::class, 'auth'])->name('auth');
+
+Route::resource('products', ProductController::class);
